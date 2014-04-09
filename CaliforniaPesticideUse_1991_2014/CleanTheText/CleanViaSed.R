54616ef8888468335bdd5ab0e725dd9c71dcce32
Andrew Defries PhD <andrew.defries@gmail.com>
2014-04-09 00:23:52 -0700

    removed decimals and commas, need to bind similar compounds into data frame

--- CaliforniaPesticideUse_1991_2014/CleanTheText/CleanViaSed.R
+++ CaliforniaPesticideUse_1991_2014/CleanTheText/CleanViaSed.R
@@ -4,33 +4,38 @@
 system("for i in *.txt 
 do
 ##########
-########################################clean out the crap
-sed '/Page/d' $i | sed '/STATE/d' | sed '/SOLD/d' | sed '/Active/d' | sed '/Total/d' | sed '/Registrants/d' | sed '/Number/d' | sed '/Chemicals/d' | sed 's/^    *$//g' | sed 's/ - //g' | sed '/^ *$/d' | sed 's/^[ \t]*//;s/[ \t]*$//' | sed 's/ *$//' > $i.Cleaner 
+########################################clean out lines containing headers and text, remove commas from values, remove some tabs and whitespace (consider removing)
+sed '/Page/d' $i | sed '/STATE/d' | sed '/SOLD/d' | sed '/Active/d' | sed '/Total/d' | sed '/Registrants/d' | sed '/Number/d' | sed '/Chemicals/d' | sed 's/^    *$//g' | sed 's/ - //g' | sed '/^ *$/d' | sed 's/^[ \t]*//;s/[ \t]*$//' | sed 's/ *$//' | sed 's/,//g' > $i.Cleaner 
 ########################################
 #######################################get just the names
 cut -c 1-74 $i.Cleaner > $i.NamesToClean 
-sed 's/^/\"/' $i.NamesToClean | sed 's/[ \t]*$//' | sed 's/$/\"/' > Names
+sed 's/^/\"/' $i.NamesToClean | sed 's/[ \t]*$//' | sed 's/$/\"/' | sed '$d' > Names
 mv Names $i.NamesToClean
 #######################################
 cut -c 79-200 $i.Cleaner > $i.NumbersToClean
 ###For testing
 #echo $i > index
 #cat index > $i.Out
+#########join cleaned files
 paste $i.NamesToClean $i.NumbersToClean > $i.Out
-sed '$d' $i.Out | sed '$d' > $i.done
+#########remove last line in file and everything after the decimal
+sed '$d' $i.Out | sed s/\\.[^\\.]*$// > $i.done
+#####
 #sed -e '/./!d' $i.Out | sed '$d' | sed '$d' > $i.done
 #######Fix output manually for line wrap problem
 #pico $i.Out
 #######
 done")
 ###########
-
 files<-list.files(pattern=".done", recursive=F)
-
+#
 Load<-function(a){
 #################
-graph<-read.table(files[a], fill=TRUE, header=F, dec=".", as.is=T)
+graph<-read.table(files[a], fill=TRUE, header=F)
+######### 
+rownames(graph)<-graph[,1]
 colnames(graph)<-c("compound", "registrants", "pesticides_used")
+########
 pdf(gsub(".txt.done", ".pdf", files[a]))
 plot(rownames(graph), graph$pesticides_used, type='h', main=paste("Pesticides used in CA", gsub(".txt.done", "", files[a]), ""), xlab="compound", ylab="pesticides applied (lb)")
 dev.off()
