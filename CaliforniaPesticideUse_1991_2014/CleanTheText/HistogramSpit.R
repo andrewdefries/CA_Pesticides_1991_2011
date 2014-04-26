50a79e1d1dbd228b98e7f4a25c8b4fb518047fc2
Andrew Defries PhD <andrew.defries@gmail.com>
2014-04-26 09:48:23 -0700

    histograms work now to export png and match names from smi.png

--- CaliforniaPesticideUse_1991_2014/CleanTheText/HistogramSpit.R
+++ CaliforniaPesticideUse_1991_2014/CleanTheText/HistogramSpit.R
@@ -1,19 +1,22 @@
 graph<-read.table("Ready")
+colnames(graph)<-c("compound", "lbs", "year")
 Index<-unique(graph[1])
 write.table(Index, file="IndexList.txt", quote=T)
+################
 
-
+#######
 pdf(file="Test.pdf")
 IndexList<-read.table("IndexList.txt")
-IndexList<-c(IndexList[1])
+IndexList<-do.call(paste, as.list(IndexList))
 ##########
 GetThoHistogram<-function(d){
 ##
-Piece<-subset(graph, unique(graph[1])==IndexList[d])
+Piece<-subset(graph, unique(graph$compound)==IndexList[d])   # unique(graph[1])==IndexList[d])
+#Piece<-graph[unique(graph$compound)==IndexList[d], ]
 colnames(Piece)<-c("compound", "lbs", "year")
 ##
-#plot(as.numeric(Piece$year), as.numeric(Piece$lbs), type="h")
-hist(as.numeric(Piece$year), freq=T, probability=Piece$lbs, xlab="year", ylab="lbs", xlim=c(0,30), main="Test")
+plot(as.numeric(Piece$year), as.numeric(Piece$lbs), type="h", main=IndexList[d])
+hist(as.numeric(Piece$year), freq=T, probability=Piece$lbs, xlab="year", ylab="lbs", xlim=c(0,30), main=IndexList[d])
 ##
 }
 ##########
