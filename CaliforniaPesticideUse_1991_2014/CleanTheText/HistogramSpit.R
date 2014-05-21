a393b5a946580deba4089e3ecbad02265efd9688
Andrew Defries PhD <andrew.defries@gmail.com>
2014-05-20 19:03:06 -0700

    need to fix year using regex [0-9]x4 to plot properly

--- CaliforniaPesticideUse_1991_2014/CleanTheText/HistogramSpit.R
+++ CaliforniaPesticideUse_1991_2014/CleanTheText/HistogramSpit.R
@@ -1,11 +1,17 @@
 graph<-read.table("Ready")
 colnames(graph)<-c("compound", "lbs", "year")
 Index<-unique(graph[1])
+#Year<-unique(graph[3])
+
+#run on CMD line ls *[0-9].txt | sed 's/pdsd//g' | sed 's/.txt//g' > YearIndex
+Year<-read.table("YearIndex")
+year<-seq_along(Year)
+
 write.table(Index, file="IndexList.txt", quote=T)
 ################
 
 #######
-pdf(file="Test.pdf")
+pdf(file="Test2.pdf")
 IndexList<-read.table("IndexList.txt")
 IndexList<-do.call(paste, as.list(IndexList))
 ##########
@@ -15,8 +21,9 @@ Piece<-subset(graph, unique(graph$compound)==IndexList[d])   # unique(graph[1])=
 #Piece<-graph[unique(graph$compound)==IndexList[d], ]
 colnames(Piece)<-c("compound", "lbs", "year")
 ##
+plot(as.numeric(Piece$year), as.numeric(Piece$lbs), type="p", main=IndexList[d])
 plot(as.numeric(Piece$year), as.numeric(Piece$lbs), type="h", main=IndexList[d])
-hist(as.numeric(Piece$year), freq=T, probability=Piece$lbs, xlab="year", ylab="lbs", xlim=c(0,30), main=IndexList[d])
+hist(as.numeric(Piece$year), freq=T, probability=Piece$lbs, xlab="year", ylab="lbs", xlim=c(0,ceiling(year)), main=IndexList[d])
 ##
 }
 ##########
